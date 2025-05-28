91eca01fb71682f0b6aacaa695a8466624d5cc28
ST10441724-Aadi-Mohanlal <163293806+ST10441724-Aadi-Mohanlal@users.noreply.github.com>
2025-05-29 00:04:14 +0200

    Add files via upload

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿
+using IceTask2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceTask2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IceTask2.Models
+{
+    public class Student : Controller
+    {
+        public class Students
+        {
+            public int Id { get; set; }
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public int Age { get; set; }
+        }
+
+    }
+}
